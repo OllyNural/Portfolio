e295506cdcd376c38d4cf108c632a3b967710ed1
Oliver Nural <onural@and.digital>
2019-06-04 15:54:12 +0100

    Changed color of pwa and logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `The Junior Dev`,
+        short_name: `The Junior Dev`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#004E89`,
+        theme_color: `#004E89`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tjd.png`, // This path is relative to the root of the site.
       },
     },
     {
